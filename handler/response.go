5d5eb6bac504ea14d4cf5cd1198e5e921046e494
Neville Guimarães <nevilleguimaraes@gmail.com>
2026-06-25 20:19:40 -0300

    feat: add request and response helpers

---
+++ handler/response.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, code int, op string, data any) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message": fmt.Sprintf("operation from handler: %s successfull", op),
+		"data":    data,
+	})
+}
