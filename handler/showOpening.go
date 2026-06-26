8735bc1e065754eb123cc41221b5a396826514f9
Neville Guimarães <nevilleguimaraes@gmail.com>
2026-06-25 21:59:16 -0300

    feat: add show opening handler logic

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,29 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/leoneville/gopportunities/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	idParam := ctx.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		sendError(ctx, http.StatusBadRequest, "invalid id type")
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	err = db.WithContext(ctx).First(&opening, id).Error
+	if err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %v not found", id))
+		return
+	}
+
+	sendSuccess(ctx, http.StatusOK, "show-opening", opening)
 }
